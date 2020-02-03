1e7aa7772516be21db49a007409c1f0741c80e6c
juliotakers1 <goustering@hotmail.com>
2020-02-02 20:21:44 -0600

    PERIODO

--- vistas/registrarTa.php
+++ vistas/registrarTa.php
@@ -5,20 +5,20 @@ $codigo=$_POST["codigo"];
 $codigod=$_POST["codigod"];
 $codigot=$_POST["codigot"];
 $cantidad=$_POST["cantidad"];
-$fechaI=$_POST["fechai"];
-$fechaF=$_POST["fechaf"];
+$periodo=$_POST["periodo"];
 
 
 
 
-$sentencia=$base_de_datos->prepare("INSERT INTO reservacion(CODIGO_RESERVACION, CODIGO_DOCENTE, CODIGO_TABLET, CANTIDAD, FECHA_INICIO, FECHA_FIN)
-VALUES(:codigo,:codigod,:codigot,:cantidad,:fechai,:fechaf);");
+
+$sentencia=$base_de_datos->prepare("INSERT INTO reservacion(CODIGO_RESERVACION, CODIGO_DOCENTE, CODIGO_TABLET, CANTIDAD, PERIODO)
+VALUES(:codigo,:codigod,:codigot,:cantidad,:periodo);");
 $sentencia->bindParam(':codigo',$codigo);
 $sentencia->bindParam(':codigod',$codigod);
 $sentencia->bindParam(':codigot',$codigot);
 $sentencia->bindParam(':cantidad',$cantidad);
-$sentencia->bindParam(':fechai',$fechaI);
-$sentencia->bindParam(':fechaf',$fechaF);
+$sentencia->bindParam(':periodo',$periodo);
+
 
 if($sentencia->execute()){
     return header("Location:AgregarReserva.php");   
